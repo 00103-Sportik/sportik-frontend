4033c31e72307939f463976dbfc582f53604bf89
Ilia Kniazev [student] <knyazev.ilya.sergeevich@gmail.com>
2023-12-12 13:50:47 +0300

    Add page for exercise create

---
+++ src/common/validations/exerciseValidationSchema.ts
@@ -0,0 +1,33 @@
+import * as Yup from 'yup';
+
+const MIN_NAME_LENGTH = 4;
+const MAX_NAME_LENGTH = 32;
+const MAX_DESCRIPTION_LENGTH = 255;
+
+export type ExerciseFields = {
+  name: string;
+  description: string;
+  type: string;
+  subtype: string;
+  combinationParams: string;
+};
+
+export const exerciseInitialValue: ExerciseFields = {
+  name: '',
+  description: '',
+  type: '',
+  subtype: '',
+  combinationParams: '',
+};
+
+export const exerciseValidationSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(MIN_NAME_LENGTH, `Incorrect name: min length ${MIN_NAME_LENGTH} characters`)
+    .max(MAX_NAME_LENGTH, `Incorrect name: max length ${MAX_NAME_LENGTH} characters`)
+    .required('Required field'),
+  description: Yup.string().max(
+    MAX_DESCRIPTION_LENGTH,
+    `Incorrect description: max length ${MAX_NAME_LENGTH} characters`,
+  ),
+  combinationParams: Yup.string().required('Required field'),
+});
--- src/store/workouts/workouts.selectors.ts
+++ src/store/workouts/workouts.selectors.ts
@@ -2,5 +2,8 @@ import { AppRootStateType } from '../types.ts';
 
 export const selectWorkoutsCount = (state: AppRootStateType) => state.workouts.count;
 export const selectCurrentWorkout = (state: AppRootStateType) => state.workouts.id;
+export const selectType = (state: AppRootStateType) => state.workouts.type;
+export const selectSubtype = (state: AppRootStateType) => state.workouts.subtype;
+
 export const selectExercises = (state: AppRootStateType) => state.workouts.exercises;
 export const selectFullWorkoutInfo = (state: AppRootStateType) => state.workouts;
