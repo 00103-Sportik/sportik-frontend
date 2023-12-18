ea244692a26f986dfab55f878d70f4a6369c548e
Ilia Kniazev [student] <knyazev.ilya.sergeevich@gmail.com>
2023-12-18 05:45:31 +0300

    Fix approaches validation

--- src/common/validations/approachValidationSchema.ts
+++ src/common/validations/approachValidationSchema.ts
@@ -4,24 +4,30 @@ import { combinationParams } from '../types/workouts.ts';
 export const approachInitialValue = [
   {
     name: combinationParams[0].name,
-    initial: {
-      param1: '0',
-      param2: '00:00:00',
-    },
+    initial: [
+      {
+        param1: '0',
+        param2: '00:00:00',
+      },
+    ],
   },
   {
     name: combinationParams[1].name,
-    initial: {
-      param1: '0',
-      param2: '0',
-    },
+    initial: [
+      {
+        param1: '0',
+        param2: '0',
+      },
+    ],
   },
   {
     name: combinationParams[2].name,
-    initial: {
-      param1: '0',
-      param2: '00:00:00',
-    },
+    initial: [
+      {
+        param1: '0',
+        param2: '00:00:00',
+      },
+    ],
   },
 ];
 
@@ -29,7 +35,7 @@ export const approachValidationSchema = [
   {
     name: combinationParams[0].name,
     approach: Yup.object().shape({
-      fields: Yup.array().of(
+      initial: Yup.array().of(
         Yup.object().shape({
           param1: Yup.string()
             .matches(/^\d{1,3}(\.\d)?$/, {
@@ -48,26 +54,28 @@ export const approachValidationSchema = [
   {
     name: combinationParams[1].name,
     approach: Yup.object().shape({
-      fields: Yup.array().of(
-        Yup.object().shape({
-          param1: Yup.string()
-            .matches(/^\d{1,4}$/, {
-              message: `Count must be an integer. Max - 9999`,
-            })
-            .required('Required field'),
-          param2: Yup.string()
-            .matches(/^\d{1,3}(\.\d)?$/, {
-              message: `Weight must be an integer or fractional with one decimal place. Max - 999.9`,
-            })
-            .required('Required field'),
-        }),
+      initial: Yup.array().of(
+        Yup.object()
+          .shape({
+            param1: Yup.string()
+              .matches(/^\d{1,4}$/, {
+                message: `Count must be an integer. Max - 9999`,
+              })
+              .required('Required field'),
+            param2: Yup.string()
+              .matches(/^\d{1,3}(\.\d)?$/, {
+                message: `Weight must be an integer or fractional with one decimal place. Max - 999.9`,
+              })
+              .required('Required field'),
+          })
+          .required('fdsfsfs'),
       ),
     }),
   },
   {
     name: combinationParams[2].name,
     approach: Yup.object().shape({
-      fields: Yup.array().of(
+      initial: Yup.array().of(
         Yup.object().shape({
           param1: Yup.string()
             .matches(/^\d{1,4}$/, {
--- src/pages/approaches/Approaches.tsx
+++ src/pages/approaches/Approaches.tsx
@@ -8,7 +8,7 @@ import { useAppDispatch, useAppSelector } from '../../store/hooks.ts';
 import { selectCurrentWorkout, selectExercises } from '../../store/workouts/workouts.selectors.ts';
 import { setApproaches } from '../../store/workouts/workouts.slice.ts';
 import { approachInitialValue, approachValidationSchema } from '../../common/validations/approachValidationSchema.ts';
-import { Field, FieldArray, FieldProps, Form, Formik } from 'formik';
+import { useFormik } from 'formik';
 import { AiOutlineClose, AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 import { IMask } from 'react-imask';
 import { WORKOUTS_URL } from '../../common/constants/api.ts';
@@ -22,7 +22,50 @@ function Approaches() {
   const exercise = exercisesFromStore.filter((exercise) => exercise.uuid === uuid)[0];
   const [check, setCheck] = useState(true);
   const combParams = combinationParams.filter((params) => params.params === exercise.combination_params)[0];
-  const [approaches, setApproachesToState] = useState<ApproachState[]>([]);
+  const intialVal = approachInitialValue.filter((init) => init.name === combParams.name)[0];
+  const addApproach = () => {
+    if (formik.values.initial.length === 0) {
+      const toApproaches: ApproachState = { param1: intialVal.initial[0].param1, param2: intialVal.initial[0].param2 };
+      formik.setValues({ name: '', initial: [toApproaches] });
+    } else {
+      formik.setValues({
+        name: '',
+        initial: [...formik.values.initial, { ...formik.values.initial[formik.values.initial.length - 1] }],
+      });
+    }
+  };
+
+  const onSubmit = () => {
+    let toApproaches: ApproachRequest[];
+    if (combParams.param2 === 'Time') {
+      toApproaches = formik.values.initial.map((approach) => {
+        if (approach.param2 !== '') {
+          const parts = approach.param2.split(':');
+          const time = Number(parts[0]) * 3600 + Number(parts[1]) * 60 + Number(parts[2]);
+          return { param1: Number(approach.param1), param2: time } as ApproachRequest;
+        } else {
+          return { param1: Number(approach.param1), param2: 0 } as ApproachRequest;
+        }
+      });
+    } else {
+      toApproaches = formik.values.initial.map((approach) => {
+        return { param1: Number(approach.param1), param2: Number(approach.param2) } as ApproachRequest;
+      });
+    }
+    dispatch(
+      setApproaches({
+        exerciseId: uuid || '',
+        approaches: toApproaches,
+      }),
+    );
+    navigate(`${WORKOUTS_URL}/${workout_uuid}`);
+  };
+
+  const formik = useFormik({
+    initialValues: intialVal,
+    onSubmit,
+    validationSchema: approachValidationSchema.filter((valSchema) => valSchema.name === combParams.name)[0].approach,
+  });
 
   const getApproachesState = (approachesFromState: ApproachRequest[]) => {
     let toApproaches: ApproachState[] = [];
@@ -51,210 +94,139 @@ function Approaches() {
   };
 
   const dispatch = useAppDispatch();
-  const intialVal = approachInitialValue.filter((init) => init.name === combParams.name)[0].initial;
   const navigate = useNavigate();
   const workout_uuid = useAppSelector(selectCurrentWorkout);
 
   useEffect(() => {
     if (check) {
-      setApproachesToState(getApproachesState(exercise.approaches || []));
+      formik.setValues({ name: '', initial: getApproachesState(exercise.approaches || []) });
       setCheck(false);
     }
   }, [check]);
 
-  const onSubmit = () => {
-    if (approaches.length === 0) {
-      const toApproaches: ApproachState = { param1: intialVal.param1, param2: intialVal.param2 };
-      setApproachesToState([toApproaches]);
-    } else {
-      setApproachesToState([...approaches, { ...approaches[approaches.length - 1] }]);
-    }
-    console.log(approaches);
-  };
-
-  const saveApproaches = () => {
-    let toApproaches: ApproachRequest[];
-    if (combParams.param2 === 'Time') {
-      toApproaches = approaches.map((approach) => {
-        if (approach.param2 !== '') {
-          const parts = approach.param2.split(':');
-          const time = Number(parts[0]) * 3600 + Number(parts[1]) * 60 + Number(parts[2]);
-          return { param1: Number(approach.param1), param2: time } as ApproachRequest;
-        } else {
-          return { param1: Number(approach.param1), param2: 0 } as ApproachRequest;
-        }
-      });
-    } else {
-      toApproaches = approaches.map((approach) => {
-        return { param1: Number(approach.param1), param2: Number(approach.param2) } as ApproachRequest;
-      });
-    }
-    dispatch(
-      setApproaches({
-        exerciseId: uuid || '',
-        approaches: toApproaches,
-      }),
-    );
-    navigate(`${WORKOUTS_URL}/${workout_uuid}`);
-  };
-
   const deleteApproach = (index: number) => {
-    const tempApproaches = [...approaches];
+    const tempApproaches = [...formik.values.initial];
     tempApproaches.splice(index, 1);
-    setApproachesToState(tempApproaches);
+    formik.setValues({ name: '', initial: tempApproaches });
   };
 
   return (
     <>
       <p className={styles.p1}>{data?.data.name || ''}</p>
-      <Formik
-        initialValues={intialVal}
-        onSubmit={onSubmit}
-        validationSchema={
-          approachValidationSchema.filter((valSchema) => valSchema.name === combParams.name)[0].approach
-        }
-      >
-        {(props) => (
-          <Form>
-            <FieldArray
-              name="friends"
-              render={() => (
-                <div>
-                  <div className={styles.sideButtonsBox}>
-                    <p className="text-[22px]">{`Approaches: ${approaches.length}`}</p>
-                    <button className="btn-black mt-0" type="submit">
-                      <AiOutlinePlus />
-                    </button>
-                    <button
-                      className="btn-black mt-0"
-                      disabled={approaches.length === 0}
-                      type="button"
-                      onClick={() => {
-                        setApproachesToState(approaches.splice(0, approaches.length - 1));
-                      }}
-                    >
-                      <AiOutlineMinus />
-                    </button>
-                  </div>
-                  <div className={styles.mainBox}>
-                    {approaches.length !== 0 ? (
-                      approaches.map((approach, index) => (
-                        <div className={styles.itemBox}>
-                          <div key={index} className={styles.boxItems}>
-                            <div className={styles.boxContent}>
-                              <div className={styles2.boxInfo}>
-                                <Field name={`fields.${index}.param1`}>
-                                  {({ meta, field }: FieldProps) => (
-                                    <Input
-                                      type="text"
-                                      {...field}
-                                      value={approach.param1}
-                                      onChange={(e) => {
-                                        const tempApproach = { ...approach };
-                                        tempApproach.param1 = e.target.value;
-                                        const tempApproaches = [...approaches];
-                                        tempApproaches[index] = tempApproach;
-                                        setApproachesToState(tempApproaches);
-                                        props.handleChange(e);
-                                      }}
-                                      placeholder={combParams.param1}
-                                      error={meta.touched && !!meta.error}
-                                      errorText={meta.error}
-                                      className="form-input-narrow w-[150px]"
-                                    ></Input>
-                                  )}
-                                </Field>
-                                <Field name={`fields.${index}.param2`}>
-                                  {({ meta, field }: FieldProps) =>
-                                    combParams.param2 === 'Time' ? (
-                                      <Input
-                                        id={`time.${index}`}
-                                        type="text"
-                                        {...field}
-                                        value={approach.param2}
-                                        onChange={(e) => {
-                                          const tempApproach = { ...approach };
-                                          const element = document.getElementById(`time.${index}`);
-                                          const maskOptions = {
-                                            mask: '00:00:00',
-                                          };
-                                          // @ts-ignore
-                                          IMask(element, maskOptions);
-                                          tempApproach.param2 = e.target.value;
-                                          const tempApproaches = [...approaches];
-                                          tempApproaches[index] = tempApproach;
-                                          setApproachesToState(tempApproaches);
-                                          props.handleChange(e);
-                                        }}
-                                        placeholder={combParams.param2}
-                                        error={meta.touched && !!meta.error}
-                                        errorText={meta.error}
-                                        className="form-input-narrow w-[150px]"
-                                      ></Input>
-                                    ) : (
-                                      <Input
-                                        type="text"
-                                        {...field}
-                                        value={approach.param2}
-                                        onChange={(e) => {
-                                          const tempApproach = { ...approach };
-                                          tempApproach.param2 = e.target.value;
-                                          const tempApproaches = [...approaches];
-                                          tempApproaches[index] = tempApproach;
-                                          setApproachesToState(tempApproaches);
-                                          props.handleChange(e);
-                                        }}
-                                        placeholder={combParams.param2}
-                                        error={meta.touched && !!meta.error}
-                                        errorText={meta.error}
-                                        className="form-input-narrow w-[150px]"
-                                      ></Input>
-                                    )
-                                  }
-                                </Field>
-                              </div>
-                              <div className={styles.sideButtonsBox}>
-                                <button type="button" onClick={() => deleteApproach(index)}>
-                                  <AiOutlineClose />
-                                </button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      ))
-                    ) : (
-                      <h1 className={styles.noEntities}>There are no approaches yet</h1>
-                    )}
-                  </div>
-                  <div className={styles.buttonsBox}>
-                    <button
-                      className="btn-black"
-                      onClick={() => {
-                        if (!props.isValid && !!props.submitCount) {
-                          toast('Please enter a valid parameters!', {
-                            position: 'top-center',
-                            autoClose: 1000,
-                            hideProgressBar: false,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: 'dark',
-                          });
-                        } else {
-                          saveApproaches();
+      <form id="approach-form" onSubmit={formik.handleSubmit}>
+        <div className={styles.sideButtonsBox}>
+          <p className="text-[22px]">{`Approaches: ${formik.values.initial.length}`}</p>
+          <button className="btn-black mt-0" onClick={addApproach} type="reset">
+            <AiOutlinePlus />
+          </button>
+          <button
+            className="btn-black mt-0"
+            disabled={formik.values.initial.length === 0}
+            type="button"
+            onClick={() => {
+              formik.setValues({
+                name: '',
+                initial: formik.values.initial.splice(0, formik.values.initial.length - 1),
+              });
+            }}
+          >
+            <AiOutlineMinus />
+          </button>
+        </div>
+        <div className={styles.mainBox}>
+          {formik.values.initial.length !== 0 ? (
+            formik.values.initial.map((approach, index) => (
+              <div className={styles.itemBox}>
+                <div key={index} className={styles.boxItems}>
+                  <div className={styles.boxContent}>
+                    <div className={styles2.boxInfo}>
+                      <Input
+                        type="text"
+                        {...formik.getFieldProps(`initial[${index}].param1`)}
+                        error={
+                          formik.getFieldMeta(`initial[${index}].param1`).touched &&
+                          !!formik.getFieldMeta(`initial[${index}].param1`).error
                         }
-                      }}
-                    >
-                      Save
-                    </button>
+                        errorText={formik.getFieldMeta(`initial[${index}].param1`).error}
+                        onChange={(e) => {
+                          const tempApproach = { ...approach };
+                          tempApproach.param1 = e.target.value;
+                          const tempApproaches = [...formik.values.initial];
+                          tempApproaches[index] = tempApproach;
+                          formik.setValues({ name: '', initial: tempApproaches });
+                          formik.handleChange(e);
+                        }}
+                        placeholder={combParams.param1}
+                        className="form-input-narrow w-[150px]"
+                      ></Input>
+                      {combParams.param2 === 'Time' ? (
+                        <Input
+                          id={`time.${index}`}
+                          type="text"
+                          {...formik.getFieldProps(`initial[${index}].param2`)}
+                          error={
+                            formik.getFieldMeta(`initial[${index}].param2`).touched &&
+                            !!formik.getFieldMeta(`initial[${index}].param2`).error
+                          }
+                          errorText={formik.getFieldMeta(`initial[${index}].param2`).error}
+                          onChange={(e) => {
+                            const tempApproach = { ...approach };
+                            const element = document.getElementById(`time.${index}`);
+                            const maskOptions = {
+                              mask: '00:00:00',
+                            };
+                            // @ts-ignore
+                            IMask(element, maskOptions);
+                            tempApproach.param2 = e.target.value;
+                            const tempApproaches = [...formik.values.initial];
+                            tempApproaches[index] = tempApproach;
+                            formik.setValues({ name: '', initial: tempApproaches });
+                            formik.handleChange(e);
+                          }}
+                          placeholder={combParams.param2}
+                          className="form-input-narrow w-[150px]"
+                        ></Input>
+                      ) : (
+                        <Input
+                          type="text"
+                          {...formik.getFieldProps(`initial[${index}].param2`)}
+                          error={
+                            formik.getFieldMeta(`initial[${index}].param2`).touched &&
+                            !!formik.getFieldMeta(`initial[${index}].param2`).error
+                          }
+                          errorText={formik.getFieldMeta(`initial[${index}].param2`).error}
+                          onChange={(e) => {
+                            const tempApproach = { ...approach };
+                            tempApproach.param2 = e.target.value;
+                            const tempApproaches = [...formik.values.initial];
+                            tempApproaches[index] = tempApproach;
+                            formik.setValues({ name: '', initial: tempApproaches });
+                            formik.handleChange(e);
+                          }}
+                          placeholder={combParams.param2}
+                          className="form-input-narrow w-[150px]"
+                        ></Input>
+                      )}
+                    </div>
+                    <div className={styles.sideButtonsBox}>
+                      <button type="reset" onClick={() => deleteApproach(index)}>
+                        <AiOutlineClose />
+                      </button>
+                    </div>
                   </div>
                 </div>
-              )}
-            />
-          </Form>
-        )}
-      </Formik>
+              </div>
+            ))
+          ) : (
+            <h1 className={styles.noEntities}>There are no approaches yet</h1>
+          )}
+        </div>
+        <div className={styles.buttonsBox}>
+          <button className="btn-black" type="submit" form="approach-form">
+            Save
+          </button>
+        </div>
+      </form>
     </>
   );
 }
